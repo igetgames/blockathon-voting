4cdb30c3040d941fe87a8a94413f465071c4a9b7
Marcus R. Brown <me@igetgam.es>
2018-04-28 14:44:56 -0700

    Wrap the page in a section.

--- src/components/Root.js
+++ src/components/Root.js
@@ -4,6 +4,7 @@ import Anchor from 'grommet/components/Anchor';
 import App from 'grommet/components/App';
 import Article from 'grommet/components/Article';
 import Header from 'grommet/components/Header';
+import Section from 'grommet/components/Section';
 import Menu from 'grommet/components/Menu';
 import Router, { Routes } from './Router';
 import sitemap from '../sitemap';
@@ -22,6 +23,17 @@ const menuAnchors = sitemap.reduce((anchors, content) => {
   return anchors;
 }, []);
 
+const RootSection = ({ children, ...props }) => (
+  <Section
+    justify='center'
+    align='center'
+    pad={{vertical: 'large'}}
+    {...props}
+  >
+    {children}
+  </Section>
+);
+
 export default () => (
   <BrowserRouter>
     <App>
@@ -31,7 +43,9 @@ export default () => (
             {menuAnchors}
           </Menu>
         </Header>
-        <Router routes={<Routes sitemap={sitemap} />} />
+        <RootSection>
+          <Router routes={<Routes sitemap={sitemap} />} />
+        </RootSection>
       </Article>
     </App>
   </BrowserRouter>
