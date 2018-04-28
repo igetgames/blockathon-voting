194dc6b9cfdb2daeb58b901fca95dd0f6ab92946
Marcus R. Brown <me@igetgam.es>
2018-04-28 11:07:17 -0700

    Stub required pages.

--- src/sitemap.js
+++ src/sitemap.js
@@ -1,7 +1,22 @@
 import Home from './pages/Home';
+import Teams from './pages/Teams';
+import Vote from './pages/Vote';
+import VotingCategories from './pages/VotingCategories';
 
 export default [
   {
     component: Home
+  },
+  {
+    component: Teams,
+    path: 'teams'
+  },
+  {
+    component: VotingCategories,
+    path: 'categories'
+  },
+  {
+    component: Vote,
+    path: 'vote'
   }
 ];
