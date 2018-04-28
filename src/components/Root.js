b3fd1d97ab81877b9a515bae7307e1236dd43a40
Marcus R. Brown <me@igetgam.es>
2018-04-28 12:16:02 -0700

    Integrate Grommet.

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import App from 'grommet/components/App';
 import Router, { Routes } from './Router';
 import sitemap from '../sitemap';
 
 export default () => (
   <BrowserRouter>
-    <Router routes={<Routes sitemap={sitemap} />} />
+    <App>
+      <Router routes={<Routes sitemap={sitemap} />} />
+    </App>
   </BrowserRouter>
 );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { DrizzleProvider } from 'drizzle-react';
 import { LoadingContainer } from 'drizzle-react-components';
+import 'grommet/grommet.min.css';
 import './index.css';
 import Root from './components/Root';
 import registerServiceWorker from './registerServiceWorker';
